625f19a0ac095e1ccde2629d27eb43e17ad2ffec
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2022-11-25 16:47:31 +0000

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import Directory from "./component/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
+const Shop = () => {
+	return <h1>Shop</h1>;
+};
 const App = () => {
-	return <Directory />;
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<Navigation />}
+			>
+				<Route
+					index
+					element={<Home />}
+				/>
+				<Route
+					path="shop"
+					element={<Shop />}
+				/>
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
